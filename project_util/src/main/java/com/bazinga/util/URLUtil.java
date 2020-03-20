3e409b82719434854e0cc4c9ca9f9e9d223d663b
jun_li <jun_li@baofu.com>
2020-03-20 14:16:37 +0800

    工具

--- project_util/src/main/java/com/bazinga/util/URLUtil.java
+++ project_util/src/main/java/com/bazinga/util/URLUtil.java
@@ -1,15 +1,14 @@
 package com.bazinga.util;
 
-import java.io.UnsupportedEncodingException;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 //URL字符串转码
 public class URLUtil {
     public static void main(String[] args) {
         try {
-            System.out.println(URLDecoder.decode("http%3A%2F%2Fw.baofoo.net%2Fpages%2Fviewpage.action%3FpageId%3D8074823%26tdsourcetag%3Ds_pcqq_aiomsg"));
+            System.out.println(new SimpleDateFormat("yyyyMMdd").format(new Date()));
         } catch (Exception e) {
             e.printStackTrace();
         }
